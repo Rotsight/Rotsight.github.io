9b7aa324e23dbd9b38a25128658213c59d296c53
rem-bryan <68305923+rem-bryan@users.noreply.github.com>
2025-10-17 19:44:59 +0000

    Deploying to gh-pages from @ rem-bryan/rem-bryan.github.io@c9d6390c7fd0ecc4cb9594d51137479a1541b681 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Rotsight/Rotsight.github.io",
+    "data-repo": "rem-bryan/rem-bryan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
